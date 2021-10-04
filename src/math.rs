87645ac012cfed9de1d916b816847e56c2181f0c
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2021-10-04 21:53:41 +0200

    feat: implement vector operations

--- src/math.rs
+++ src/math.rs
@@ -11,6 +11,25 @@ impl Vector {
         }
     }
 
+    pub fn div(&self, number: &f64) -> Vector {
+        Vector {
+            x: self.x / number,
+            y: self.y / number,
+        }
+    }
+
+    pub fn magnitude(&self) -> f64 {
+        let length: f64 = self.x * self.x + self.y * self.y;
+        length.sqrt()
+    }
+
+    pub fn mult(&self, number: &f64) -> Vector {
+        Vector {
+            x: self.x * number,
+            y: self.y * number,
+        }
+    }
+
     pub fn sub(&self, vector: &Vector) -> Vector {
         Vector {
             x: self.x - vector.x,
@@ -43,5 +62,28 @@ mod tests {
         let new_location = location.sub(&Vector { x: 5.0, y: 5.0 });
         assert!(new_location.x == -4.0);
         assert!(new_location.y == 10.0);
+    }
+
+    #[test]
+    fn should_multiply_vectors() {
+        let location = Vector { x: 14.0, y: 5.0 };
+        let scaled_location = location.mult(&3.0);
+        assert!(scaled_location.x == 42.0);
+        assert!(scaled_location.y == 15.0);
+    }
+
+    #[test]
+    fn should_divide_vectors() {
+        let location = Vector { x: 14.0, y: 5.0 };
+        let scaled_location = location.div(&2.0);
+        assert!(scaled_location.x == 7.0);
+        assert!(scaled_location.y == 2.5);
+    }
+
+    #[test]
+    fn should_calculate_magnitute() {
+        let location = Vector { x: 10.0, y: 5.0 };
+        let magnitude = location.magnitude();
+        assert!(magnitude == 11.180339887498949);
     }
 }
