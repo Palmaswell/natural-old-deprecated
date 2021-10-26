30778cea32fb2188932e7d331eda57567035901a
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2021-10-26 21:24:46 +0200

    feat: implement mover

--- src/mover.rs
+++ src/mover.rs
@@ -1,7 +1,5 @@
 mod math;
 pub use crate::math::Vector;
-use rand::Rng;
-use wasm_bindgen::prelude::*;
 
 pub struct Mover {
     pub location: Vector,
@@ -30,31 +28,5 @@ impl Mover {
     pub fn update(&mut self) {
         self.location = self.location.add(&self.velocity);
         self.constrain();
-    }
-}
-
-#[wasm_bindgen]
-pub fn run(width: u32, height: u32) {
-    let mut rng = rand::thread_rng();
-    let initial_location = Vector { x: 100.0, y: 500.0 };
-    let initial_velocity = Vector {
-        x: rng.gen_range(-2.0..2.0),
-        y: rng.gen_range(-2.0..2.0),
-    };
-
-    let mut mover = Mover {
-        location: initial_location,
-        velocity: initial_velocity,
-        canvas_width: width,
-        canvas_height: height,
-    };
-
-    loop {
-        mover.velocity.add(&Vector {
-            x: rng.gen_range(-2.0..2.0),
-            y: rng.gen_range(-2.0..2.0),
-        });
-        mover.update();
-        println!("x {} y {}", mover.location.x, mover.location.y);
     }
 }
